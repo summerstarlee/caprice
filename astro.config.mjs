73c7daf879b5228b076c896993c87b53dbb7d511
lishun_li <lishun_li@youzan.com>
2023-10-11 19:24:21 +0800

    添加官网 demo 文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [tailwind(), preact()],
+  output: "hybrid",
   adapter: netlify()
 });
